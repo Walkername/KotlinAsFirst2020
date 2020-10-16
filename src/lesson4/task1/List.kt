47f9a268a03ae77b1cf701f1ebc9b650ef3e9bca
Timur Magomedov <timurmag2020@gmail.com>
2020-10-16 20:18:46 +0300

    Lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -3,6 +3,7 @@
 package lesson4.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
 import kotlin.math.sqrt
 
 // Урок 4: списки
@@ -120,14 +121,32 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    var sum = 0.0
+    if (v.isEmpty()) return 0.0
+    for (i in 0 until v.size) {
+        val element = v[i]
+        sum += sqr(element)
+    }
+    return sqrt(sum)
+}
 
 /**
  * Простая (2 балла)
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    var k = 0
+    var sum = 0.0
+    if (list.isEmpty()) return 0.0
+    for (i in 0 until list.size) {
+        val element = list[i]
+        k++
+        sum += element
+    }
+    return sum / k
+}
 
 /**
  * Средняя (3 балла)
@@ -137,7 +156,15 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    val mlist = mean(list)
+    if (list.isNotEmpty()) {
+        for (i in 0 until list.size) {
+            list[i] -= mlist
+        }
+    }
+    return list
+}
 
 /**
  * Средняя (3 балла)
@@ -146,7 +173,14 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = TODO()
+fun times(a: List<Int>, b: List<Int>): Int {
+    var sum = 0
+    if (a.isEmpty()) return 0
+    for (i in 0 until a.size) {
+        sum += (a[i] * b[i])
+    }
+    return sum
+}
 
 /**
  * Средняя (3 балла)
